e0bda732acaf21815348c2f7b600e9948fe7edf7
G6EJD <d.l.bird@sky.com>
2017-12-23 13:16:45 +0000

    Add files via upload

---
+++ ESP8266_Erase_Credentials_Factory_Default.ino
@@ -0,0 +1,20 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+  resetToFactoryDefaults();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+void resetToFactoryDefaults() {
+  WiFi.disconnect(); // Erases WiFi Credentials
+  delay(3000);
+  ESP.reset();
+  delay(3000);
+  ESP.eraseConfig();
+  delay(3000);
+}
+
+
